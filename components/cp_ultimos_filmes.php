f49fad77e74287ce423b90d8ec7a37916f4d84bf
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-08 12:35:26 +0100

    Fix link

--- components/cp_ultimos_filmes.php
+++ components/cp_ultimos_filmes.php
@@ -1,6 +1,6 @@
 <?php
 // conexão à base de dados
-include_once "connections/connection.php" ;
+include_once "connections/connection.php";
 ?>
 
 <section class="sec-filmes pb-5 mt-5" id="filmes">
@@ -20,7 +20,7 @@ include_once "connections/connection.php" ;
             $stmt = mysqli_stmt_init($link);
 
             // Define the query
-            $query = "SELECT titulo, capa , tipo FROM filmes INNER JOIN generos ON generos.id_generos = filmes.ref_generos ORDER BY ano DESC LIMIT 3;";
+            $query = "SELECT id_filmes, titulo, capa , tipo FROM filmes INNER JOIN generos ON generos.id_generos = filmes.ref_generos ORDER BY ano DESC LIMIT 3;";
 
             // Prepare the statement
             if (mysqli_stmt_prepare($stmt, $query)) {
@@ -28,30 +28,28 @@ include_once "connections/connection.php" ;
                 // Execute the prepared statement
                 if (mysqli_stmt_execute($stmt)) {
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $titulo, $capa, $genero);
+                    mysqli_stmt_bind_result($stmt, $id, $titulo, $capa, $genero);
 
                     // Fetch values
                     while (mysqli_stmt_fetch($stmt)) {
-                        ?>
+            ?>
                         <div class="col-md-4 mb-md-0 pb-5">
-                        <div class="card pb-2 h-100 shadow rounded">
-                            <div class="capas-preview" style="background-image: url(<?php echo "imgs/capas/" . $capa; ?>)"></div>
+                            <div class="card pb-2 h-100 shadow rounded">
+                                <div class="capas-preview" style="background-image: url(<?php echo "imgs/capas/" . $capa; ?>)"></div>
                                 <div class="card-body text-center">
-                                <h4 class="text-uppercase m-0 mt-2"><?php echo "<h2>$titulo</h2>"; ?></h4>
-                                <hr class="my-3 mx-auto" />
-                                <div class="tipo-filme mb-0 small text-black-50"><?php  echo "<p>$genero</p>"; ?></div>
-                                <a href="filme_detail.php" class="mt-2 btn btn-outline-primary"><b><i class="fas fa-plus text-primary"></i></b></a>
+                                    <h4 class="text-uppercase m-0 mt-2"><?php echo "<h2>$titulo</h2>"; ?></h4>
+                                    <hr class="my-3 mx-auto" />
+                                    <div class="tipo-filme mb-0 small text-black-50"><?php echo "<p>$genero</p>"; ?></div>
+                                    <a href="filme_detail.php?id=<?php echo $id; ?>" class="mt-2 btn btn-outline-primary"><b><i class="fas fa-plus text-primary"></i></b></a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <?php
+            <?php
                     }
-
                 } else {
                     // Execute error
                     echo "Error: " . mysqli_stmt_error($stmt);
                 }
-
             } else {
                 // Errors related with the query
                 echo "Error: " . mysqli_error($link);
