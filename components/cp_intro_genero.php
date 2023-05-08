78d300ca3939f5ce30b1111dc3aca13e4e4849a7
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-08 17:51:12 +0100

    Add voltar button

--- components/cp_intro_genero.php
+++ components/cp_intro_genero.php
@@ -1,43 +1,43 @@
 <?php
 
-            if (isset($_GET["id"])) {
-                $id = $_GET["id"];
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
 
-                $link = new_db_connection();
-                $stmt = mysqli_stmt_init($link);
+    $link = new_db_connection();
+    $stmt = mysqli_stmt_init($link);
 
-                $query = "SELECT tipo 
+    $query = "SELECT tipo 
                           FROM generos
                           WHERE generos.id_generos = ?";
 
-                if (mysqli_stmt_prepare($stmt, $query)) {
+    if (mysqli_stmt_prepare($stmt, $query)) {
 
-                    mysqli_stmt_bind_param($stmt, 'i', $id);
+        mysqli_stmt_bind_param($stmt, 'i', $id);
 
-                    mysqli_stmt_execute($stmt);
-                    mysqli_stmt_bind_result($stmt, $tipo);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $tipo);
 
-                    mysqli_stmt_store_result($stmt);
+        mysqli_stmt_store_result($stmt);
 
-                    if (mysqli_stmt_num_rows($stmt) > 0 ) {
+        if (mysqli_stmt_num_rows($stmt) > 0) {
 
-                        while (mysqli_stmt_fetch($stmt)) {
-
-                            echo '<h1>' . $tipo . '</h1>';
-                        }
-                    } else {
-                        echo "<h1>Ainad não temos filmes deste género";
-                    }
-
-                    mysqli_stmt_close($stmt);
-                } else {
-                    echo mysqli_error($link);
-                }
-
-                mysqli_close($link);
-            } else {
-                echo "<h1>Falta o id do género!!!</h1>";
+            while (mysqli_stmt_fetch($stmt)) {
+                echo '<a class="btn btn-info my-3" href="generos.php">Voltar</a>';
+                echo '<h1>' . $tipo . '</h1>';
             }
+        } else {
+            echo "<h1>Ainad não temos filmes deste género";
+        }
+
+        mysqli_stmt_close($stmt);
+    } else {
+        echo mysqli_error($link);
+    }
+
+    mysqli_close($link);
+} else {
+    echo "<h1>Falta o id do género!!!</h1>";
+}
 ?>
 
 <p class="text-black-60 text-left pb-4">
