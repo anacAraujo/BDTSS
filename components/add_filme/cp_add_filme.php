7173ea93165ba8b52cc390bbc0e5f31524c639d1
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-08 17:53:55 +0100

    Delete white space

--- components/add_filme/cp_add_filme.php
+++ components/add_filme/cp_add_filme.php
@@ -10,6 +10,12 @@ include_once "connections/connection.php";
 
         <!-- Formulário -->
         <div class="row">
+            <?php
+            $link = new_db_connection();
+            $stmt = mysqli_stmt_init($link);
+
+            $query = "SELECT id_generos, tipo FROM generos";
+            ?>
             <form class="col-6" action="./scripts/filmes/sc_add_filme.php" method="post" class="was-validated">
                 <div class="mb-3 mt-3"><label for="uname" class="form-label">Título:*</label><input type="text" class="form-control" id="titulo" value="" name="titulo" required>
                     <div class="valid-feedback">Valid.</div>
@@ -24,8 +30,20 @@ include_once "connections/connection.php";
                     <div class="invalid-feedback">Please fill out this field.</div>
                 </div>
                 <div class="mb-3 mt-3"><label for="uname" class="form-label">Género:</label><select class="form-select" id="genero" name="genero" required>
-                        <option value="">Escolha um género</option>
-                        <option value="x">Género X</option>
+                        <option value=''> Escolha uma opção </option>
+                        <?php
+                        if (mysqli_stmt_prepare($stmt, $query)) {
+                            mysqli_stmt_execute($stmt);
+                            mysqli_stmt_bind_result($stmt, $id_generos, $tipo);
+                            while (mysqli_stmt_fetch($stmt)) {
+                                echo "<option value='$id_generos'> $tipo </option>";
+                            }
+                            mysqli_stmt_close($stmt);
+                        } else {
+                            echo mysqli_error($link);
+                        }
+                        mysqli_close($link);
+                        ?>
                     </select>
                     <div class="valid-feedback">Valid.</div>
                     <div class="invalid-feedback">Please fill out this field.</div>
@@ -37,7 +55,9 @@ include_once "connections/connection.php";
                 <div class="mb-3 mt-3"><label for="uname" class="form-label">URL Trailer:</label><input type="url" class="form-control" id="url_trailer" value="" name="url_trailer">
                     <div class="valid-feedback">Valid.</div>
                     <div class="invalid-feedback">Please fill out this field.</div>
-                </div><button type="submit" class="btn btn-primary">Inserir</button>
+                </div>
+                <a class="btn btn-info my-3" href="index.php">Cancelar</a>
+                <button type="submit" class="btn btn-primary">Inserir</button>
             </form>
         </div>
     </div>
