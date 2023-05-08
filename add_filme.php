81515dd373368e4c858666718c6ba5a5afb94253
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-08 13:07:56 +0100

    Create file

---
+++ add_filme.php
@@ -0,0 +1,11 @@
+<!-- Head -->
+<?php include_once "./components/cp_head.php" ?>
+
+<!-- Navigation-->
+<?php include_once "./components/cp_navbar.php" ?>
+
+<!-- Adicionar filme -->
+<?php include_once "./components/add_filme/cp_add_filme.php" ?>
+
+<!-- Rodapé -->
+<?php include_once "./components/cp_footer.php" ?>
