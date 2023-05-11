048e06f537c5ae34409989267f82634f05324ff7
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-11 13:20:21 +0100

    Create file

---
+++ scripts/favoritos/sc_delete_favorito.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once "../../connections/connection.php";
+
+if (isset($_GET['id']) && isset($_SESSION['id'])) {
+    $id_filme = $_GET['id'];
+    $user = $_SESSION['id'];
+
+    $link = new_db_connection();
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "DELETE FROM filmes_favoritos WHERE ref_utilizadores = ? AND ref_filmes = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+        /* Bind paramenters */
+        mysqli_stmt_bind_param($stmt, "ii", $user, $id_filme);
+        /* execute the prepared statement */
+        if (!mysqli_stmt_execute($stmt)) {
+            /* Como prevenir? */
+            echo "Error:" . mysqli_stmt_error($stmt);
+        } else {
+            /* Cidade apagada */
+
+            header("Location: ../../filmes.php");
+        }
+    } else {
+        echo ("Error description: " . mysqli_error($link));
+    }
+    /* close statement */
+    mysqli_stmt_close($stmt);
+    /* close connection */
+    mysqli_close($link);
+}
+
+
+// header("Location: ../../filme_detail.php.php");
