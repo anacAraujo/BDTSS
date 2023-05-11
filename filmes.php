e0ef9d83ee4eb35758a674412bcf8483d2157393
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-11 17:18:25 +0100

    Fix verification

--- filmes.php
+++ filmes.php
@@ -5,13 +5,7 @@
 <?php include_once "./components/cp_navbar.php" ?>
 
 <!-- Filmes -->
-<?php 
-    if (!isset($_POST['pesquisa']) || empty($_POST['pesquisa'])) {
-        include_once "./components/cp_lista_filmes_v3.php";
-    } else {
-        include_once "./components/cp_filmes_resultados.php";
-    }
-?>
+<?php include_once "./components/cp_lista_filmes_v3.php"; ?>
 
 <!-- Rodapé -->
 <?php include_once "./components/cp_footer.php" ?>
