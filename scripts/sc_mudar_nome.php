2c4ff0ad2e1b29e0d12adba908c98abee7b78a75
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-14 00:20:22 +0100

    Add file

---
+++ scripts/sc_mudar_nome.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+var_dump($_POST["username"]);
+var_dump($_SESSION);
+
+if (isset($_POST["username"]) && ($_POST["username"] != "") && (isset($_SESSION["id"]))) {
+    $username = $_POST["username"];
+    $id = $_SESSION["id"];
+
+    // We need the function!
+    require_once("../../connections/connection.php");
+
+    // Create a new DB connection
+    $link = new_db_connection();
+
+    /* create a prepared statement */
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "UPDATE utilizadores
+              SET login = ?, nome = ?
+              WHERE id_utilizadores = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+        /* Bind paramenters */
+        mysqli_stmt_bind_param($stmt, "ssi", $username, $username, $id);
+        /* execute the prepared statement */
+        if (!mysqli_stmt_execute($stmt)) {
+            echo "Error:" . mysqli_stmt_error($stmt);
+        } else {
+            /* Update done */
+            echo "aaaa";
+            //   header("Location: ../../index.php");
+        }
+    } else {
+        echo ("Error description: " . mysqli_error($link));
+    }
+    /* close statement */
+    mysqli_stmt_close($stmt);
+    /* close connection */
+    mysqli_close($link);
+} else
+    echo "erro no POST";
