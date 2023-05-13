e92bdab2e14efd9fb41bbdd5e6b7c93c6f39077e
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-05-14 00:21:27 +0100

    Add form change username

--- components/cp_logout.php
+++ components/cp_logout.php
@@ -1,11 +1,20 @@
 <section class="sec-filmes pb-5" id="lista-filmes">
     <div class="container px-lg-5 pt-3">
 
-        <div class="row">
-            <h1>Tem a certeza que quer sair?</h1>
+        <div class="row my-5">
+            <form action="./scripts/sc_mudar_nome.php" method="post">
+                <div class="mb-3 mt-3">
+                    <label for="uname" class="form-label">Comentar:</label>
+                    <textarea class="form-control" id="username" value="" name="username" rows="1"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Mudar username</button>
+            </form>
+        </div>
 
+        <div class="row">
+            <h4>Tem a certeza que quer sair?</h4>
             <div class="col-8">
-                <a href="./scripts/sc_logout.php"><button type="submit" class="btn btn-primary">Sair</button></a>
+                <a href="./scripts/sc_logout.php"><button type="submit" class="btn btn-primary">Logout</button></a>
             </div>
         </div>
     </div>
